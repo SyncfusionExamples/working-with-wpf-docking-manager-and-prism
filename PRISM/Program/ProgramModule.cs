d163b8e68b235d77876d2053584964802601e06e
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2019-12-02 14:14:55 +0530

    Added docking manager sample for prism

---
+++ PRISM/Program/ProgramModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Program
+{
+    public class ProgramModule: IModule
+    {
+        private readonly IRegionManager regionManager;
+        public ProgramModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(ProgramView));
+        }
+    }
+}
